98d328f87c9e50ff446e60c926de999c7cb0e190
BerkanHRGL <329918@student.mboutrecht.nl>
2022-02-23 14:09:27 +0100

    Super User kan categorieen beheren

---
+++ beheren.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Magazijn</title>
+  </head>
+  <body>
+    <h1>Categorieen beheren</h1>
+
+    <hr>
+
+    <div class="container">
+  <div class="row">
+    <div class="col-12">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Artikelnaam</th>
+            <th scope="col">Locatie</th>
+            <th scope="col">Aantal</th>
+            <th scope="col">Acties</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Categorie 1</td>
+            <td>Klaslokaal 1</td>
+            <td>5</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Categorie 2</td>
+            <td>Klaslokaal 2</td>
+            <td>2</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Categorie 3</td>
+            <td>Klaslokaal 3</td>
+            <td>3</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <th scope="row">4</th>
+            <td>Categorie 4</td>
+            <td>Klaslokaal 4</td>
+            <td>4</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr><th scope="row">5</th>
+            <td>Categorie 5</td>
+            <td>Klaslokaal 5</td>
+            <td>5</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr><th scope="row">6</th>
+            <td>Categorie 6</td>
+            <td>Klaslokaal 6</td>
+            <td>6</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr><th scope="row">7</th>
+            <td>Categorie 7</td>
+            <td>Klaslokaal 7</td>
+            <td>7</td>
+            <td>
+            <button type="button" class="btn btn-outline-dark">Wijzigen</button>
+            <button type="button" class="btn btn-outline-dark">Verwijderen</button>
+            </td>
+          </tr>
+          <tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<div class="button" style="padding-left: 110px; ">
+<li href= "beheren.php" button type="button"class="btn btn-outline-dark">Nieuwe categorie toevoegen</li>
+
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
