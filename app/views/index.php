5b47e805e07349a92351ac0bf1c58ecebc90189d
mohamedloul1 <loulmohamed346@gmail.com>
2022-03-08 12:43:26 +0100

    MVC, Pages,
    mvd framework created, links to other page also created. buttons added instead of login system

---
+++ app/views/index.php
@@ -0,0 +1,26 @@
+<?php
+/* This is how you echo out database information on the screen
+foreach ($data['users'] as $user) {
+    echo "Information: " . $user->user_name . $user->user_email;
+    echo "<br>";
+}
+*/
+?>
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1><?php echo $data['description']; ?>
+</h1>
+
+<div class="users_buttens">
+    <div class="container">
+        <div class="text-center">        
+              <a class="btn btn-success" href="<?php echo URLROOT; ?>super_user/index">Super User</a>
+              <a class="btn btn-danger" href="<?php echo URLROOT; ?>">User</a>
+              <a class="btn btn-warning" href="<?php echo URLROOT; ?>">User</a>
+
+        </div>
+
+    </div>
+
+</div>
+
+
