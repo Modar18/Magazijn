8687b23a1df8d9568fcd50469a11c3fdd018e49a
Kapello <330990@student.mboutrecht.nl>
2022-03-13 15:16:01 +0100

    Added file connect to connect to database
    db_connect added and a page with a table in it to be filled by the database

---
+++ connect_db/connect_db.php
@@ -0,0 +1,9 @@
+<?php
+  define("SERVERNAME", "localhost");
+  define("USERNAME", "Magazijn");
+  define("PASSWORD", "");
+  define("DBNAME", "Magazijn");
+
+  $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+  $sql = "SELECT * FROM `` ";
+?>
---
+++ overzicht.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+include("./connect_db.php");
+?>
+
+
+
+
+<table>
+
+</table>
+
+
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
